e67f439bca85bc72508abdc85358ee4426edfb56
evblance <evblance@users.noreply.github.com>
2019-05-27 15:56:55 +1000

    Implements on-the-fly colour processing on canvas image preview

--- webcam-shooter.js
+++ webcam-shooter.js
@@ -33,6 +33,24 @@ const photoBtn = document.getElementById('photoBtn');
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 
+// Capture references to our control sliders to allow manipulation of the rendered webcam stream
+const redMinCtrl = document.getElementById('redMinCtrl');
+const greenMinCtrl = document.getElementById('greenMinCtrl');
+const blueMinCtrl = document.getElementById('blueMinCtrl');
+const redMaxCtrl = document.getElementById('redMaxCtrl');
+const greenMaxCtrl = document.getElementById('greenMaxCtrl');
+const blueMaxCtrl = document.getElementById('blueMaxCtrl');
+
+// We keep our colour configuration from input values in a sealed object
+const colourConfig = Object.seal({
+  redMin: 0,
+  redMax: 255,
+  greenMin: 0,
+  greenMax: 255,
+  blueMin: 0,
+  blueMax: 255
+});
+
 // Setup canvas dimensions
 const ctx = canvas.getContext('2d');
 const canvasWidth = canvas.clientWidth;
@@ -50,15 +68,16 @@ let snapshotIndex = 0;
 
 /**
  * Draws the current frame being streamed on the video element onto
- * the canvas and requests the next frame to be drawn if the webcam 
- * is considered to be streaming.
+ * the canvas, triggers image processing preview, and requests the
+ * next frame to be drawn if the webcam is considered to be streaming.
  */
 const renderStreamToCanvas = () => {
   ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight);
+  processImage();
   if (webcamState === EWebcamState.STREAMING) {
     animationFrameId = window.requestAnimationFrame(renderStreamToCanvas);
   }
-}
+};
 
 /**
  * Starts the webcam stream if possible and renders it as the srcObject
@@ -95,14 +114,13 @@ const pushSnapshotToReel = () => {
   // NOTE: We assume that snapshots are the only children of the snapshot holder
   const imageURL = canvas.toDataURL('image/jpeg');
   snapshotHolder.childNodes[snapshotIndex].style.backgroundImage = `url(${imageURL})`;
-}
+};
 
 /**
  * Takes a snapshot of the current frame being streamed from the webcam.
  */
 const takePhoto = () => {
   window.cancelAnimationFrame(animationFrameId);
-  ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight);
   pushSnapshotToReel();
   windReel();
   setTimeout(renderStreamToCanvas, 2000);
@@ -114,17 +132,62 @@ const takePhoto = () => {
  */
 const windReel = () => {
   snapshotIndex = ((snapshotIndex + 1) % FILMREEL_LENGTH);
-}
+};
+
+/**
+ * Sets up our colour slider event listners to write their values to the
+ * corresponding property on the global colourConfig object.
+ */
+const initSliderEventListeners = () => {
+  redMinCtrl.addEventListener('change', (event) => colourConfig.redMin = parseInt(event.target.value));
+  greenMinCtrl.addEventListener('change', (event) => colourConfig.greenMin = parseInt(event.target.value));
+  blueMinCtrl.addEventListener('change', (event) => colourConfig.blueMin = parseInt(event.target.value));
+  redMaxCtrl.addEventListener('change', (event) => colourConfig.redMax = parseInt(event.target.value));
+  greenMaxCtrl.addEventListener('change', (event) => colourConfig.greenMax = parseInt(event.target.value));
+  blueMaxCtrl.addEventListener('change', (event) => colourConfig.blueMax = parseInt(event.target.value));
+};
+
+/**
+ * Processes the image preview rendered on the canvas based on
+ * on our colour slider input values.
+ */
+const processImage = () => {
+  // Pull image data and create a local reference for pixel-level manipulation
+  const imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  const pixelData = imageData.data;
+  
+  /**
+   * Loop over clamped UINT8 array [R, G, B, A, ...] of pixel intensity values in steps of 4
+   * to process colour based on minimum and maximum channel values obtained from our UI colour
+   * sliders.
+   */
+  const length = imageData.data.length;
+  for (let i = 0; i < length; i+=4) {
+    cursorIndex = i;
+    // Process red channel
+    pixelData[cursorIndex] = Math.max(Math.min(pixelData[cursorIndex], colourConfig.redMax), colourConfig.redMin);
+    cursorIndex++;
+    // Process green channel
+    pixelData[cursorIndex] = Math.max(Math.min(pixelData[cursorIndex], colourConfig.greenMax), colourConfig.greenMin);
+    cursorIndex++;
+    // Process blue channel
+    pixelData[cursorIndex] = Math.max(Math.min(pixelData[cursorIndex], colourConfig.blueMax), colourConfig.blueMin);
+  }
+
+  // Place the modified image data on the canvas to show processed image in photo preview
+  ctx.putImageData(imageData, 0, 0);
+};
 
 // Listen to our button and input events to allow webcam stream capture and manipulation
 startBtn.addEventListener('click', startStream);
 stopBtn.addEventListener('click', stopStream);
 photoBtn.addEventListener('click', takePhoto);
+initSliderEventListeners();
 
 // Listen to our video play event so we can stream to the canvas
 video.addEventListener('play', () => {
   animationFrameId = window.requestAnimationFrame(renderStreamToCanvas);
-})
+});
 
 // Initialise our film reel so the user's images can be displayed
 initFilmReel();
