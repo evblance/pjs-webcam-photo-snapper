c1d832a7e5f43af5817fa0e7953ecc7c1a2031a5
evblance <evblance@users.noreply.github.com>
2019-05-27 12:37:36 +1000

    Removes a magic number in creating the filmreel snapshots

--- webcam-shooter.js
+++ webcam-shooter.js
@@ -2,6 +2,7 @@
 const SNAPSHOT_CLASSNAME = 'snapshot';
 const SNAPSHOT_ELEMENT_TYPE = 'div';
 const SNAPSHOT_HOLDER_ID = 'snapshotHolder';
+const FILMREEL_LENGTH = 24;
 
 /**
  * Creates the snapshot DOM elements in the snapshot reel for representing
@@ -9,7 +10,7 @@ const SNAPSHOT_HOLDER_ID = 'snapshotHolder';
  */
 const initFilmReel = () => {
   const snapshotHolder = document.getElementById(SNAPSHOT_HOLDER_ID);
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < FILMREEL_LENGTH; i++) {
     const snapshot = document.createElement(SNAPSHOT_ELEMENT_TYPE);
     snapshot.classList.add(SNAPSHOT_CLASSNAME);
     snapshotHolder.appendChild(snapshot);
